5a7bb880aa0ed5c7a326e0be5430d5c6e4f4efb2
Roberto Alonso <roberto.alonso2628@gmail.com>
2026-02-13 18:43:06 +0100

    Prueba API

--- src/main/java/com/robertoalonso/tema4gradle/Main.java
+++ src/main/java/com/robertoalonso/tema4gradle/Main.java
@@ -1,17 +1,19 @@
 package com.robertoalonso.tema4gradle;
 
+import dev.langchain4j.model.openai.OpenAiChatModel;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+    public static void main(String[] args) {
+        final String TOKEN = "PEGA_AQUI_TU_TOKEN";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
-        }
+        var model = OpenAiChatModel.builder()
+                .baseUrl("http://localhost:11434/v1")
+                .apiKey(TOKEN)
+                .modelName("gemma:2b")
+                .build();
+        String respuesta = model.chat("Cuéntame un chiste");
+        System.out.println(respuesta);
     }
 }
