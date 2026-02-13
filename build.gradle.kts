a9089e3f2ccd4152dae682370d8986bd7c76a6e9
Roberto Alonso <roberto.alonso2628@gmail.com>
2026-02-13 18:10:44 +0100

    Dependencias

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,13 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation(platform("dev.langchain4j:langchain4j-bom:1.10.0"))
+    implementation("dev.langchain4j:langchain4j-open-ai")
+
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
+
