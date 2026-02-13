a34c9ae90c69ea01613986db2cb5ea9c32e821bc
Roberto Alonso <roberto.alonso2628@gmail.com>
2026-02-13 20:52:01 +0100

    Debate

--- src/main/java/com/robertoalonso/tema4gradle/Main.java
+++ src/main/java/com/robertoalonso/tema4gradle/Main.java
@@ -1,37 +1,45 @@
 package com.robertoalonso.tema4gradle;
 
-import dev.langchain4j.data.message.AiMessage;
-import dev.langchain4j.data.message.ChatMessage;
-import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.data.message.*;
 import dev.langchain4j.model.openai.OpenAiChatModel;
-
 import java.util.ArrayList;
 import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
     public static void main(String[] args) {
-        // El TOKEN no es necesario para interactuar con modelos locales
-        var model = OpenAiChatModel.builder()
+
+        // IA 1 (pregunta)
+        var ia1 = OpenAiChatModel.builder()
                 .baseUrl("http://localhost:11434/v1")
                 .apiKey("dummy")
                 .modelName("gemma:2b")
                 .build();
 
-        List<ChatMessage> history = new ArrayList<>();
+        // IA 2 (respuesta)
+        var ia2 = OpenAiChatModel.builder()
+                .baseUrl("http://localhost:11434/v1")
+                .apiKey("dummy")
+                .modelName("gemma:2b")
+                .build();
 
-        // Primera interacción
-        history.add(new UserMessage("Hola, soy Roberto"));
-        AiMessage respuesta = model.chat(history).aiMessage();
-        history.add(respuesta);
+        List<ChatMessage> history1 = new ArrayList<>();
+        history1.add(new SystemMessage("Eres una IA curiosa que hace preguntas sobre el valor el oro en la economia."));
 
-        // Segunda interacción
-        history.add(new UserMessage("¿Te acuerdas de como me llamo?"));
-        respuesta = model.chat(history).aiMessage();
-        history.add(respuesta);
+        history1.add(new UserMessage("Haz una pregunta."));
+        AiMessage pregunta = ia1.chat(history1).aiMessage();
 
+        System.out.println("Pregunta de la IA 1");
+        System.out.println(pregunta.text());
+
+        // Historial IA 2
+        List<ChatMessage> history2 = new ArrayList<>();
+        history2.add(new SystemMessage("Eres una IA experta en economia, sobre todo del oro"));
+
+        history2.add(new UserMessage(pregunta.text()));
+        AiMessage respuesta = ia2.chat(history2).aiMessage();
+
+        System.out.println("\nRespuesta de la IA 2:");
         System.out.println(respuesta.text());
     }
-
 }
